f341b867776289fcb1805f7963e323671f00a634
Richard <richard.kwasnicki@gmx.de>
2016-12-05 17:56:52 +0100

    upload tag analyzation

--- ml.py
+++ ml.py
@@ -20,7 +20,7 @@ key = config.key
 
 def readInUsers(cursor, limit=2000):
 	userList = []
-	query = """Select * from user where loccountrycode !='' and realname != "" LIMIT %s;""" % limit
+	query = """Select * from user where loccountrycode !='' LIMIT %s;""" % limit
 	cursor.execute(query)
 	userList = cursor.fetchall()
 	for user in userList:
@@ -64,7 +64,7 @@ def getGameTagDict(cursor):
 	cursor.execute(query)
 	gameTags = cursor.fetchall()
 	for tag in gameTags:
-		gameTagDict[tag["id"]] = tag["gameTags"]
+		gameTagDict[tag["id"]] = tag["gameTags"].split(",")
 	return gameTagDict
 
 
@@ -78,9 +78,10 @@ def readInGameTags(userList, cursor):
 			gameList = [int(game) for game in gameList if game.isdigit()]
 			for game in gameList:
 				if game in gameTagDict:
-					userTagDict[user].append(gameTagDict[game])
+					userTagDict[user].extend(gameTagDict[game])
 	return userTagDict
 	
+# TODO:	
 # Nur gametags von spielen mit mehr als 0 gespielt
 #
 #
@@ -90,40 +91,63 @@ def predictLand(userList,cursor):
 	userTagDict = readInGameTags(userIdList, cursor)
 	X = []
 	y = []
+
+	laenderTags = defaultdict(lambda: [])
 	for user in userList:
 		steamId = user["steamid"]
 		if str(steamId) in userTagDict:
 			userTags = userTagDict[steamId]
-			X.append(''.join(userTags))
+			# threshold fuer anzahl userTags?
+			X.append(' '.join(userTags[0]))
 			y.append(user["loccountrycode"])
+			laenderTags[user["loccountrycode"]].extend(userTags)
+
+	usedTags = []
+	countryDictList = []
+	with open("tag_ana_country.txt", "w") as f:
+		for country in laenderTags:
+			countryTagDict = defaultdict(lambda:0)
+			for tag in laenderTags[country]:
+				countryTagDict[tag] += 1
+				if tag not in usedTags:
+					usedTags.append(tag)
+			countryDictList.append((country,countryTagDict))
+			for tag in countryTagDict:
+				f.write(str(country)+"\t"+str(tag) + "\t" + str(countryTagDict[tag])+"\n")
+
+	with open("tag_ana_tag.txt", "w") as f:
+		for tag in usedTags:
+			for country, countryTagDict in countryDictList:
+				if tag in countryTagDict:
+					f.write(str(tag) + "\t" + str(country) + "\t" + str(countryTagDict[tag])+"\n")
+
+
 	print len(X),len(y)
+	# SVM, NB, AB, RF
+	# params = dict(
+ #       clf__C = [0.1, 0.2, 0.3, 0.5, 1.0, 10, 100],
+ #       clf__gamma = [1E-3, 1E-2, 1E-1, 1.0],
+ #   )
+
 	
-	params = dict(
-       clf__C = [0.1, 0.2, 0.3, 0.5, 1.0, 10, 100],
-       clf__gamma = [1E-3, 1E-2, 1E-1, 1.0],
-   )
+	# pipe = Pipeline([('clf', SVC())])
+	# clf = GridSearchCV(pipe, param_grid=params, cv=10)
 
-	#pipe = Pipeline([('clf', SVC())])
-	#clf = GridSearchCV(pipe, param_grid=params, cv=10)
+	# for i in range(1):
+	# 	count_vect = CountVectorizer()
+	# 	X = count_vect.fit_transform(X)
 
-	for i in range(1):
-		count_vect = CountVectorizer()
-		X = count_vect.fit_transform(X)
+	# 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.1)
 
-		X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.1)
+	# 	#clf = SVC()
+	# 	clf.fit(X_train, y_train)
+	# 	predictions = clf.predict(X_test)
+	# 	for i in range(len(X_test)):
+	# 		print X_test[i], y_test[i], predictions[i]
+	# 	print(clf.best_params_)
 
-		
-		#tfidf_transformer = TfidfTransformer()
-		#X_train_tfidf = tfidf_transformer.fit_transform(X_train)
-
-		clf = SVC()
-		clf.fit(X_train, y_train)
-		predictions = clf.predict(X_test)
-		#for i in range(len(X_test)):
-		#	print X_test[i], y_test[i], predictions[i]
-
-		score = accuracy_score(y_test, predictions)
-		print score
+	# 	score = accuracy_score(y_test, predictions)
+	# 	print score
 
 
 
@@ -132,5 +156,5 @@ def predictLand(userList,cursor):
 
 connection = pymysql.connect(host=config.db_ip, port=int(config.db_port), user=config.db_user, passwd=config.db_pass, db="steamrec", autocommit = True, cursorclass=pymysql.cursors.DictCursor)
 cursor = connection.cursor()
-userList = readInUsers(cursor)
+userList = readInUsers(cursor, limit=80000)
 predictLand(userList, cursor)
